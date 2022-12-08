aee3cdb2908eaf31b4c69e5016ff246c8cc6f8d2
Walker <walker.m.oliver@gmail.com>
2022-12-08 16:53:06 -0500

    Delete file

--- src/UI/helpers/showShipOnGrid.js
+++
@@ -1,5 +0,0 @@
-export default function showShipOnGrid(coord, shipType) {
-  const board = document.querySelector('.active-grid');
-  const holes = board.querySelectorAll('.grid-hole');
-  holes[coord].classList.add(`${shipType}`);
-}
