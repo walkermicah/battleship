3f027a915ae663aec0b605c580c6b1f47f19b06b
Walker <walker.m.oliver@gmail.com>
2022-12-09 13:56:16 -0500

    Delete

--- src/UI/helpers/showMissedTarget.js
+++
@@ -1,5 +0,0 @@
-export default function showMissedTarget(player, target) {
-  const board = document.querySelector(`.${player}-grid`);
-  const holes = board.querySelectorAll('.grid-hole');
-  holes[target].classList.add('miss');
-}
