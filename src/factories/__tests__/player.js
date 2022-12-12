4f74e1c35e6cb6ee9339f0a2ece9e8fd49a0d74c
Walker <walker.m.oliver@gmail.com>
2022-12-12 15:25:53 -0500

    Test functions to randomly place computer ships

--- src/factories/__tests__/player.js
+++ src/factories/__tests__/player.js
@@ -6,6 +6,30 @@ beforeEach(() => {
   activePlayer = player();
 });
 
+describe('randomPlay()', () => {
+  let result;
+
+  beforeEach(() => {
+    result = activePlayer.randomPlay();
+  });
+
+  it('Returns a number', () => {
+    expect(typeof result).toBe('number');
+  });
+
+  it('Returns a number between 0-99', () => {
+    expect(result).toBeGreaterThanOrEqual(0);
+    expect(result).toBeLessThanOrEqual(99);
+  });
+
+  it('Does not return the same number twice', () => {
+    const numbers = [...Array(99).keys()];
+    activePlayer.attacks.push(...numbers);
+    const number = activePlayer.randomPlay();
+    expect(number).toBe(99);
+  });
+});
+
 describe('attackEnemy()', () => {
   let attacks;
   let enemyPlayer;
@@ -13,7 +37,6 @@ describe('attackEnemy()', () => {
   const target = 25;
 
   beforeEach(() => {
-    activePlayer = player();
     attacks = activePlayer.attacks;
 
     enemyPlayer = player();
@@ -62,26 +85,131 @@ describe('attackEnemy()', () => {
   });
 });
 
-describe('randomPlay', () => {
+describe('generateCompCoords()', () => {
   let result;
+  let testShip;
 
   beforeEach(() => {
-    result = activePlayer.randomPlay();
+    result = activePlayer.generateCompCoords();
+    testShip = result.battleship;
   });
 
-  it('Returns a number', () => {
-    expect(typeof result).toBe('number');
+  it('returns an object', () => {
+    expect(typeof result).toBe('object');
   });
 
-  it('Returns a number between 0-99', () => {
-    expect(result).toBeGreaterThanOrEqual(0);
-    expect(result).toBeLessThanOrEqual(99);
+  it('returns an object with ship types as properties', () => {
+    expect(result).toHaveProperty('battleship');
   });
 
-  it('Does not return the same number twice', () => {
-    const numbers = [...Array(99).keys()];
-    activePlayer.attacks.push(...numbers);
-    const number = activePlayer.randomPlay();
-    expect(number).toBe(99);
+  it('returns an object with arrays of coordinates as values', () => {
+    expect(Array.isArray(testShip)).toBe(true);
+  });
+
+  it('returns arrays of coordinates that are the correct length', () => {
+    expect(testShip.length).toBe(4);
+  });
+
+  it('returns coordinates that are horizontally or vertically sequential', () => {
+    expect([1, 10]).toContain(testShip[1] - testShip[0]);
+
+    // If horizontal
+    if (testShip[1] - testShip[0] === 1) {
+      expect(testShip[2] - testShip[1]).toBe(1);
+      expect(testShip[3] - testShip[2]).toBe(1);
+    }
+
+    // If vertical
+    if (testShip[1] - testShip[0] === 10) {
+      expect(testShip[2] - testShip[1]).toBe(10);
+      expect(testShip[3] - testShip[2]).toBe(10);
+    }
+  });
+});
+
+describe('filterInvalidCoords()', () => {
+  const n = 2;
+
+  it('filters out the last (n - 1) columns for a horizontal ship', () => {
+    const result = activePlayer.filterInvalidCoords('horizontal', n);
+    const offBoardPositions = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99];
+    const filteredPositions = result.filter((coord) =>
+      offBoardPositions.includes(coord)
+    );
+
+    expect(filteredPositions.length).toBe(0);
+  });
+
+  it('filters out the last (n - 2) rows for a vertical ship', () => {
+    const result = activePlayer.filterInvalidCoords('vertical', n);
+    const offBoardPositions = [90, 91, 92, 93, 94, 95, 96, 97, 98, 99];
+    const filteredPositions = result.filter((coord) =>
+      offBoardPositions.includes(coord)
+    );
+
+    expect(filteredPositions.length).toBe(0);
+  });
+});
+
+describe('filterOverlapCoords', () => {
+  let availableHCoords;
+
+  beforeEach(() => {
+    availableHCoords = activePlayer.filterInvalidCoords('horizontal', 2);
+  });
+
+  it('filters out occupied positions', () => {
+    const occupiedCoords = [...Array(96).keys()];
+    const result = activePlayer.filterOverlapCoords(
+      availableHCoords,
+      occupiedCoords,
+      'horizontal',
+      2
+    );
+    expect(result).toEqual([96, 97, 98]);
+  });
+
+  it('filters out horizontal positions that would overlap with other ships', () => {
+    const occupiedCoords = [...Array(96).keys(), 97];
+    const result = activePlayer.filterOverlapCoords(
+      availableHCoords,
+      occupiedCoords,
+      'horizontal',
+      2
+    );
+    expect(result).toEqual([98]);
+  });
+
+  it('filters out vertical positions that would overlap with other ships', () => {
+    const availableVCoords = activePlayer.filterInvalidCoords('vertical', 2);
+    const occupiedCoords = [
+      ...Array(88).keys(),
+      90,
+      91,
+      92,
+      93,
+      94,
+      95,
+      96,
+      97,
+      98,
+    ];
+    const result = activePlayer.filterOverlapCoords(
+      availableVCoords,
+      occupiedCoords,
+      'vertical',
+      2
+    );
+    expect(result).toEqual([89]);
+  });
+});
+
+describe('positionShips()', () => {
+  it('places ship on gameboard at correct location', () => {
+    const coords = {
+      battleship: [72],
+    };
+    activePlayer.positionShips(coords);
+    expect(activePlayer.board.board[72]).toHaveProperty('type', 'battleship');
   });
 });
--- src/factories/player.js
+++ src/factories/player.js
@@ -2,11 +2,13 @@ import gameboard from './gameboard';
 
 export default function player() {
   const board = gameboard();
+  const positions = [...Array(100).keys()];
   const attacks = [];
 
   const randomPlay = () => {
-    const moves = [...Array(100).keys()];
-    const filteredMoves = moves.filter((move) => !attacks.includes(move));
+    const filteredMoves = positions.filter(
+      (position) => !attacks.includes(position)
+    );
     return filteredMoves[Math.floor(Math.random() * filteredMoves.length)];
   };
 
@@ -17,10 +19,106 @@ export default function player() {
     return result;
   };
 
+  // Helper functions for computer player ship selection
+  const filterInvalidCoords = (orientation, length) => {
+    if (orientation === 'horizontal') {
+      const filteredCoords = [];
+      for (let i = 0; i < 10; i++) {
+        filteredCoords.push(
+          positions.filter(
+            (coord) => coord >= i * 10 && coord < i * 10 + (11 - length)
+          )
+        );
+      }
+      return filteredCoords.flat();
+    }
+
+    if (orientation === 'vertical') {
+      return positions.filter((coord) => coord < 100 - (length - 1) * 10);
+    }
+  };
+
+  const filterOverlapCoords = (
+    availableCoords,
+    occupiedCoords,
+    orientation,
+    length
+  ) => {
+    const occupied = [];
+
+    availableCoords.forEach((coord) => {
+      for (let i = 0; i < length; i++) {
+        const increment =
+          orientation === 'horizontal' ? coord + i : coord + i * 10;
+        if (occupiedCoords.includes(increment)) occupied.push(coord);
+      }
+    });
+
+    return availableCoords.filter((coord) => !occupied.includes(coord));
+  };
+
+  const randomOrientation = () => {
+    const orientations = ['horizontal', 'vertical'];
+    return orientations[Math.floor(Math.random() * 2)];
+  };
+
+  const randomCoord = (coords) =>
+    coords[Math.floor(Math.random() * coords.length)];
+
+  // Generate ship positions for computer player
+  const generateCompCoords = () => {
+    const ships = {
+      carrier: 5,
+      battleship: 4,
+      cruiser: 3,
+      submarine: 3,
+      destroyer: 2,
+    };
+
+    const occupiedCoords = [];
+
+    Object.keys(ships).forEach((ship) => {
+      const orientation = randomOrientation();
+
+      const noInvalidCoords = filterInvalidCoords(orientation, ships[ship]);
+      const noOverlapCoords = filterOverlapCoords(
+        noInvalidCoords,
+        occupiedCoords,
+        orientation,
+        ships[ship]
+      );
+
+      const startCoord = randomCoord(noOverlapCoords);
+
+      const coords = [];
+
+      for (let i = 0; i < ships[ship]; i++) {
+        if (orientation === 'horizontal') coords.push(startCoord + i);
+        if (orientation === 'vertical') coords.push(startCoord + i * 10);
+      }
+
+      ships[ship] = coords;
+      occupiedCoords.push(...coords);
+    });
+
+    return ships;
+  };
+
+  // Place player's ships on gameboard
+  const positionShips = (coords = generateCompCoords()) => {
+    Object.keys(coords).forEach((ship) => {
+      board.addShipToBoard(ship, coords[ship]);
+    });
+  };
+
   return {
     board,
     attacks,
     attackEnemy,
     randomPlay,
+    generateCompCoords,
+    filterInvalidCoords,
+    filterOverlapCoords,
+    positionShips,
   };
 }
